eb9964619ad867c9e7e86d0c18c4098203a2b6d1
Travis Johnson <travisj@gmail.com>
2011-02-19 15:56:05 -0500

    blank rails apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThebetNo5::Application.config.secret_token = 'cfe72212a4abc0d89705de54b1233b1fe9234a7aa92044173267e0a3832a4ae498f2f200e395b88566bdf8d8a1eace763f86e4168f865f3655444eaee6222cfd'
